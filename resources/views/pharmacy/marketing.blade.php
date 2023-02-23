3bc992197e0a7f73242ccd7254a96f765a91b197
Lotrando <miroslav.klika@seznam.cz>
2023-02-23 13:53:29 +0100

    marketing test

--- resources/views/pharmacy/marketing.blade.php
+++ resources/views/pharmacy/marketing.blade.php
@@ -12,11 +12,148 @@
         </div>
         <div class="row">
           <div class="col-12">
-            <div class="faq-list">
-              <ul>
+            {{-- Services Section - Complete --}}
+            <section class="services" id="services">
+              <div class="container">
 
-              </ul>
-            </div>
+                <div class="row">
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch rounded-1">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Věrnostní program</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-md-0 mt-4">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Akční leták a jeho roznos v okolí lékárny</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-lg-0 mt-4">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Prodejní stojany</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-lg-0 mt-4">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>LCD monitory</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                </div>
+
+                <div class="row mt-4">
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch rounded-1">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Merchandising</h5>
+                      </div>
+                      <ul>
+                        <li>o umístění zboží dle plánogramů</li>
+                        <li>o vhodné orientační prvky v lékárně</li>
+                        <li>o prodejní zóny</li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-md-0 mt-4">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Slevové akce</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-lg-0 mt-4">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Reklama</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-lg-0 mt-4">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Promoakce</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                </div>
+
+                <div class="row mt-4">
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch rounded-1">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Prodej privátních značek</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-md-0 mt-4">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Specializované centrum</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-lg-0 mt-4">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Odborný cross-selling (příprodej)</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch mt-lg-0 mt-4">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Odborný up-selling</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                </div>
+
+                <div class="row mt-4">
+
+                  <div class="col-12 d-flex align-items-centr mt-lg-0 mt-4">
+                    <div class="icon-box">
+                      <div class="section-title">
+                        <h5>Vzorový akční leták</h5>
+                      </div>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium molestiae quis dicta minima tenetur sit ipsam magni tempore ad!</p>
+                    </div>
+                  </div>
+
+                </div>
+            </section>
+            {{-- End Services Section --}}
           </div>
         </div>
     </section>
